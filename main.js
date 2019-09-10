f7980163107f1fd19ea33634bacbb0b6212adb19
grace-mugoiri <gracemugoiri@gmail.com>
2019-09-10 14:21:58 +0300

    setup html form and js function skeleton

---
+++ main.js
@@ -0,0 +1,13 @@
+let myLibrary = [];
+
+function Book() {
+    // this.title = title
+    // this.theTitle = function() {
+    //     console.log(title)
+    // }
+}
+function addBookToLibrary() {
+
+}
+// const thebook = new Book("King Kong")
+// thebook.theTitle()
