902b3305894fa1562caedeca78fc025626df4400
grace-mugoiri <gracemugoiri@gmail.com>
2019-09-23 13:25:08 +0300

    fix linter

--- main.js
+++ main.js
@@ -1,37 +1,50 @@
-/ eslint-env browser */;
+/* eslint-env browser */
 
-const container = document.querySelector("#gridcontainer");
-const addBookButton = document.querySelector("#newBookButton");
-const inputTitle = document.querySelector("#title");
-const inputAuthor = document.querySelector("#author");
-const inputPages = document.querySelector("#pages");
-const inputRead = document.querySelector("#read");
-const addFormButton = document.querySelector("#addForm");
-const formDiv = document.querySelector("#newbook");
+const container = document.querySelector('#gridcontainer');
+const addBookButton = document.querySelector('#newBookButton');
+const inputTitle = document.querySelector('#title');
+const inputAuthor = document.querySelector('#author');
+const inputPages = document.querySelector('#pages');
+const inputRead = document.querySelector('#read');
+const addFormButton = document.querySelector('#addForm');
+const formDiv = document.querySelector('#newbook');
 
 
 const myLibrary = [];
 let initialRender = false;
-formDiv.style.display = "none";
+formDiv.style.display = 'none';
 
 // Book constructor
-function Book (title, author, pages, read, rendered) {
+function Book(title, author, pages, read, rendered) {
   this.title = title;
   this.author = author;
   this.pages = pages;
   this.read = read;
-  this.beenRead = function() {this.read = !this.read;};
+  this.beenRead = function () { this.read = !this.read; };
   this.rendered = rendered;
+}
+function beenRead(readStatusButton) {
+  const i = readStatusButton.getAttribute('data-id');
+  const book = myLibrary[i];
+  book.beenRead();
+
+  if (book.read === true) { readStatusButton.textContent = 'READ: Yes'; } else { readStatusButton.textContent = 'READ: No'; }
+}
+
+function removeBook(deleteBook) {
+  const i = deleteBook.getAttribute('data-id');
+
+  myLibrary.splice(i, 1);
 }
 
 // Display the form
-function showForm(){
-  if (formDiv.style.display === "none") { formDiv.style.display = "block"; }
-  if (addFormButton.style.display === "block") { addFormButton.style.display = "none"; }
+function showForm() {
+  if (formDiv.style.display === 'none') { formDiv.style.display = 'block'; }
+  if (addFormButton.style.display === 'block') { addFormButton.style.display = 'none'; }
 }
-addFormButton.addEventListener("click", showForm);
+addFormButton.addEventListener('click', showForm);
 
-function addBookToLibrary (book) {
+function addBookToLibrary(book) {
   myLibrary.push(book);
 }
 function resetForm() {
@@ -44,12 +57,11 @@ function resetForm() {
 
 const createBook = () => {
   let passed;
-  if ((inputTitle.value == null || inputTitle.value == "") || (inputAuthor.value == null || inputAuthor.value == "") || (inputPages.value == null || inputPages.value == "") || (isNaN(inputPages.value))) {
-    if(isNaN(inputPages.value)) {inputPages.value = "Add number of pages"; }
-    alert ("All fields must be entered");
+  if ((inputTitle.value === null || inputTitle.value === '') || (inputAuthor.value === null || inputAuthor.value === '') || (inputPages.value === null || inputPages.value === '') || (Number.isNaN(inputPages.value))) {
+    if (Number.isNaN(inputPages.value)) { inputPages.value = 'Add number of pages'; }
+    alert('All fields must be entered');
     passed = false;
-  }
-  else {
+  } else {
     let readBook = false;
     if (inputRead.checked) { readBook = true; } else { readBook = false; }
 
@@ -58,8 +70,8 @@ const createBook = () => {
       inputAuthor.value,
       inputPages.value,
       readBook,
-      false)
-    );
+      false,
+    ));
     render(myLibrary);
     passed = true;
   }
@@ -67,48 +79,38 @@ const createBook = () => {
     resetForm();
   }
 };
-addBookButton.addEventListener("click", createBook);
+addBookButton.addEventListener('click', createBook);
 
 function render(array) {
-  for(var i = array.length-1; i >=0; i--) {
-    if(array[i].rendered == false) {
-      const div = document.createElement("div");
+  for (let i = array.length - 1; i >= 0; i--) {
+    if (array[i].rendered === false) {
+      const div = document.createElement('div');
       div.dataset.id = i;
-      div.classList.add("cards");
+      div.classList.add('cards');
       div.textContent = `Book : ${array[i].title}`;
       div.textContent += `Author : ${array[i].author}`;
       div.textContent += `Pages : ${array[i].pages} pages`;
 
-      let readStatusButton = document.createElement("button");
+      const readStatusButton = document.createElement('button');
 
-      if(array[i].read == true) { readStatusButton.textContent = "READ?: Yes"; }
-      else {readStatusButton.textContent = "READ: No"; }
-      readStatusButton.classList.add("readbutton");
+      if (array[i].read === true) { readStatusButton.textContent = 'READ?: Yes'; } else { readStatusButton.textContent = 'READ: No'; }
+      readStatusButton.classList.add('readbutton');
       readStatusButton.dataset.id = i;
-      readStatusButton.addEventListener("click", beenRead);
-      function beenRead() {
-        let i = this.getAttribute("data-id");
-        let book = myLibrary[i];
-        book.beenRead();
+      readStatusButton.addEventListener('click', beenRead(readStatusButton));
 
-        if(book.read == true) {this.textContent = "READ: Yes";}
-        else {this.textContent = "READ: No"; }
-      }
       div.appendChild(readStatusButton);
 
-      let deleteBook = document.createElement("button");
-      deleteBook.textContent = "Delete Book";
-      deleteBook.classList.add("deletebutton");
+      const deleteBook = document.createElement('button');
+      deleteBook.textContent = 'Delete Book';
+      deleteBook.classList.add('deletebutton');
       deleteBook.dataset.id = i;
-      deleteBook.addEventListener("click", removeBook);
-      function removeBook() {
-        let i = this.getAttribute("data-id");
+      deleteBook.addEventListener('click', () =>{
+        removeBook(deleteBook);
         container.removeChild(div);
-        myLibrary.splice(i, 1);
-      }
+      });
+
       div.appendChild(deleteBook);
-      if (initialRender == false) {container.appendChild(div); }
-      else { container.insertBefore(div, container.children[2]); }
+      if (initialRender === false) { container.appendChild(div); } else { container.insertBefore(div, container.children[2]); }
       array[i].rendered = true;
     }
   }
